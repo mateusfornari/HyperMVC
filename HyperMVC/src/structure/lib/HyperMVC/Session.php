eb0233a3f05792c72f23f6117ed6d4779c57ca25
mateusfornari <mateusfornari@192.168.1.148>
2014-02-28 10:26:29 -0300

    Method Session::start() implemented.

--- HyperMVC/src/structure/lib/HyperMVC/Session.php
+++ HyperMVC/src/structure/lib/HyperMVC/Session.php
@@ -2,8 +2,23 @@
 
 class Session{
     
-    public static function teste(){
-        echo 'sessao';
-    }
+	public static function start($sessionName = null) {
+		
+		if(is_null($sessionName)){
+			$sessionName = 'HyperMVCSession';
+		}
+		$secure = isset($_SERVER['HTTPS']);
+		$httpOnly = true;
+		ini_set('session.use_only_cookies', 1);
+		$cookieParams = session_get_cookie_params();
+		session_set_cookie_params($cookieParams['lifetime'], $cookieParams['path'], $cookieParams['domain'], $secure, $httpOnly);
+		session_name($sessionName);
+		session_start();
+	}
+
+	
+	public static function renew() {
+		
+	}
     
 }
