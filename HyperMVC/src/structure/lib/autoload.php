2ed1c6ed64150a2981f2d84b142a3928981409e7
mateusfornari <mateusfornari@192.168.1.148>
2014-04-02 10:17:36 -0300

    Autoload changed.

--- HyperMVC/src/structure/lib/autoload.php
+++ HyperMVC/src/structure/lib/autoload.php
@@ -1,43 +1,47 @@
 <?php
-spl_autoload_register('autoloadHypermvc');
+spl_autoload_register('HyperMVCAutolaod::autoload');
 
-function autoloadHypermvc($name){
-    $name = str_replace('\\', '/', $name);
-	$lib = findDir(__DIR__.'/', $name);
-	if(!is_null($lib)){
-		require_once $lib.$name.'.php';
-        return true;
-	}
-    $model = findDir(__DIR__.'/../model/', $name);
-    if(!is_null($model)){
-        require_once $model.$name.'.php';
-        return true;
-    }
-    $controller = findDir(__DIR__.'/../controller/', $name);
-    if(!is_null($controller)){
-        require_once $controller.$name.'.php';
-        return true;
-    }
-    $vendor = findDir(__DIR__.'/../vendors/', $name);
-    if(!is_null($vendor)){
-        require_once $vendor.$name.'.php';
-        return true;
-    }
-     
-}
-
-function findDir($root, $name){
-	if(file_exists($root.$name.'.php')){
-		return $root;
-	}else{
-		$files = scandir($root);
-		$dir = null;
-		for($i = 2; $i < sizeof($files); $i++){
-			if(is_dir($root.$files[$i])){
-				$dir = findDir($root.$files[$i].'/', $name);
-				if(!is_null($dir)) return $dir;
-			}
+class HyperMVCAutolaod{
+	
+	public static function autoload($name){
+		$name = str_replace('\\', '/', $name);
+		$lib = self::findDir(__DIR__.'/', $name);
+		if(!is_null($lib)){
+			require_once $lib.$name.'.php';
+			return true;
 		}
-		return null;
+		$model = self::findDir(__DIR__.'/../model/', $name);
+		if(!is_null($model)){
+			require_once $model.$name.'.php';
+			return true;
+		}
+		$controller = self::findDir(__DIR__.'/../controller/', $name);
+		if(!is_null($controller)){
+			require_once $controller.$name.'.php';
+			return true;
+		}
+		$vendor = self::findDir(__DIR__.'/../vendors/', $name);
+		if(!is_null($vendor)){
+			require_once $vendor.$name.'.php';
+			return true;
+		}
+
 	}
+
+	private static function findDir($root, $name){
+		if(file_exists($root.$name.'.php')){
+			return $root;
+		}else{
+			$files = scandir($root);
+			$dir = null;
+			for($i = 2; $i < sizeof($files); $i++){
+				if(is_dir($root.$files[$i])){
+					$dir = self::findDir($root.$files[$i].'/', $name);
+					if(!is_null($dir)) return $dir;
+				}
+			}
+			return null;
+		}
+	}
+	
 }
